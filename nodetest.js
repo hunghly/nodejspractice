b4bd39e9574944d0e8db27b9ae8879230a9b0b47
Hung Ly <hunghly@gmail.com>
2020-05-08 14:46:55 -0500

    added timers and performed encapsulation for functions

--- nodetest.js
+++ nodetest.js
@@ -45,48 +45,53 @@ http.createServer(serverCallback).listen(8080);
 var http = require('http');
 var request = require('request');
 var fs = require('fs');
+var csv = require('csv');
+var url = require('url');
+var create_html = require('./create_html.js')
 
-var request_body = undefined;
+var json_request_body = undefined;
+var csv_request_body = undefined;
 var html_content = undefined;
 
-function createHtmlStringFromJSON(retrievedData) {
-    let body_begin_index = html_content.indexOf('<body>');
-    let body_end_index = html_content.indexOf('</body>');
 
-    let string_until_body = html_content.slice(0, body_begin_index + 6);
-    console.log(string_until_body.toString());
-    let string_from_body = html_content.slice(body_end_index);
-    let html_string = '<table>\n';
-    html_string += '<tr>\n';
-    for (var attribute in retrievedData[0]) {
-        if (typeof retrievedData[0][attribute] !== 'object') {
-            html_string += "<td>" + attribute + "</td>\n";
-        }
-    }
-    html_string += "</tr>\n";
 
-    retrievedData.forEach(function(object) {
-        html_string += '<tr>\n';
-        for (var attribute in object) {
-            if (typeof object[attribute] !== 'object') {
-                html_string += '<td>' + object[attribute] + '</td>\n';
-            }
-        }
-        html_string += "</tr>\n";
-    });
-    html_string += "</table>";
-    return string_until_body + html_string + string_from_body;
-}
+setInterval(function() {
+    request('https://www.bnefoodtrucks.com.au/api/1/trucks',
+        function(err, request_res, body) {
+            json_request_body = body;
+        });
+    console.log('requesting json data');
+}, 2000);
 
-request('https://www.bnefoodtrucks.com.au/api/1/trucks',
-    function(err, request_res, body) {
-        request_body = body;
-    });
+
+
+// https://www.data.brisbane.qld.gov.au/data/dataset/1e11bcdd-fab1-4ec5-b671-396fd1e6dd70/resource/3c972b8e-9340-4b6d-8c7b-2ed988aa3343/download/public-art-open-data-2020-01-09.csv
+
+setInterval(function() {
+    request('https://www.data.brisbane.qld.gov.au/data/dataset/1e11bcdd-fab1-4ec5-b671-396fd1e6dd70/resource/3c972b8e-9340-4b6d-8c7b-2ed988aa3343/download/public-art-open-data-2020-01-09.csv',
+        function(err, request_res, body) {
+            csv.parse(body, function(err, data) {
+                // console.log(data);
+                csv_request_body = data;
+            });
+            // request_body = body;
+        });
+    console.log('requesting csv data');
+}, 2000);
 
 http.createServer(function(req, res) {
-    if (request_body && html_content) {
+    if (json_request_body && csv_request_body && html_content) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(createHtmlStringFromJSON(JSON.parse(request_body)));
+        var request_url = url.parse(req.url);
+        console.log(request_url.path);
+        switch (request_url.path) {
+            case '/json':
+                res.end(create_html.createHtmlStringFromJSON(html_content, JSON.parse(json_request_body)));
+                break;
+            case '/csv':
+                res.end(create_html.createHtmlStringFromCsv(html_content, csv_request_body));
+                break;
+        }
     } else {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end("Nothing retrieved yet");
