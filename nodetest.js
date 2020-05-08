72f39ac2e0c4e728949ece87a619f7af248fcb26
Hung Ly <hunghly@gmail.com>
2020-05-08 11:34:14 -0500

    added/worked on node testing

---
+++ nodetest.js
@@ -0,0 +1,10 @@
+let http = require('http');
+
+function serverCallback(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello ' + process.argv[2]);
+}
+
+http.createServer(serverCallback).listen(8080);
